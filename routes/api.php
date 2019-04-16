5f8142773ae9d33ccb04ce6cf316ef2f061c5cda
petarkaranovic <petarkaranovic@yahoo.com>
2019-04-16 13:46:19 +0200

    small change in route for logout

--- routes/api.php
+++ routes/api.php
@@ -18,7 +18,7 @@ Route::group([
     'prefix' => 'auth'
 ], function ($router) {
     Route::post('login', 'AuthController@login');
-    Route::post('logout', 'AuthController@logout');
+    Route::get('logout', 'AuthController@logout');
     Route::post('refresh', 'AuthController@refresh');
     Route::post('me', 'AuthController@me');
     Route::post('register','AuthController@register');
